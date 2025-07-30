335cbbaf0ac16ed5800d53c8301549f4cf4ce2ae
Krishnakant Sahu <krishnakantsahu916@gmail.com>
2025-07-30 12:14:56 +0530

    bug fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import Hero from "@/components/Hero";
-import { Card } from "@/components/ui/card";
 import RoleCard from "@/components/RoleCard";
 
 export default function Home() {
   return (
     <div>
       <Hero/>
-      <Card/>
       <RoleCard/>
     </div>
   );
